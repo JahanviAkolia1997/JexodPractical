8d2c03bed45cf67e0f47b2e65700442b340ca3ff
jahanvi-dev <jahanvi.akolia@gmail.com>
2024-03-20 09:33:07 +0100

    add json data

---
+++ src/data.tsx
@@ -0,0 +1,184 @@
+export const actualVersion = [
+    {
+        id: 1,
+        name: 'All Articles',
+        colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+        rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+        legalEntiry: 11,
+        version: 'actual',
+        currency: 'USD',
+        children: [
+        {
+            id: 2,
+            name: 'Bikes',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 17,
+            version: 'actual',
+            currency: 'euro',
+            children: [
+            {
+            id: 4, 
+            name: 'Mountain Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue'}, 
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 15,
+            version: 'actual',
+            currency: 'USD',
+            },
+            { 
+            id: 5, 
+            name: 'Road Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 19,
+            version: 'actual',
+            currency: 'euro',
+            },
+            { 
+            id: 6, 
+            name: 'Touring Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue'},
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 11,
+            version: 'actual',
+            currency: 'USD',
+            }]
+        },
+        { 
+            id: 3, 
+            name: 'Complementary Products',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 19,
+            version: 'actual',
+            currency: 'euro',
+            children: [
+            { 
+                id: 7, 
+                name: 'Subscription', 
+                colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+                rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+                legalEntiry: 11,
+                version: 'actual',
+                currency: 'USD',
+            }
+            ]
+        }
+        ]
+    },
+];
+export const budgetVersion = [
+    {
+        id: 10,
+        name: 'Publication',
+        colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+        rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+        legalEntiry: 11,
+        version: 'budget',
+        currency: 'USD',
+        children: [
+        {
+            id: 2,
+            name: 'Books',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 11,
+            version: 'budget',
+            currency: 'USD',
+        }
+        ]
+    }
+];
+export const usdCurrency = [
+    {
+        id: 10,
+        name: 'Publication',
+        colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+        rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+        legalEntiry: 11,
+        version: 'actual',
+        currency: 'USD',
+        children: [
+        {
+            id: 2,
+            name: 'Books',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 11,
+            version: 'actual',
+            currency: 'USD',
+        }
+        ]
+    }
+];
+export const euroVersion = [
+    {
+        id: 1,
+        name: 'All Articles',
+        colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+        rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+        legalEntiry: 11,
+        version: 'actual',
+        currency: 'euro',
+        children: [
+        {
+            id: 2,
+            name: 'Bikes',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 17,
+            version: 'actual',
+            currency: 'euro',
+            children: [
+            {
+            id: 4, 
+            name: 'Mountain Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue'}, 
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 15,
+            version: 'actual',
+            currency: 'euro',
+            },
+            { 
+            id: 5, 
+            name: 'Road Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 19,
+            version: 'actual',
+            currency: 'euro',
+            },
+            { 
+            id: 6, 
+            name: 'Touring Bikes', 
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue'},
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 11,
+            version: 'actual',
+            currency: 'euro',
+            }]
+        },
+        { 
+            id: 3, 
+            name: 'Complementary Products',
+            colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+            rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+            legalEntiry: 19,
+            version: 'actual',
+            currency: 'euro',
+            children: [
+            { 
+                id: 7, 
+                name: 'Subscription', 
+                colValue: { col1: 'Units', col2: 'Unit Price', col3: 'Gross Revenue' },
+                rowData: { europe: 59.37, germany: 10.33, freiburg: 3.2132, berlin: 3423.34, greatbritain: 23.33 },
+                legalEntiry: 11,
+                version: 'actual',
+                currency: 'euro',
+            }
+            ]
+        }
+        ]
+    },
+];
